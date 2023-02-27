ded98fea537490c55410bb913677f92064cd9cfa
kaitlinflinn <81885499+kaitlinflinn@users.noreply.github.com>
2023-02-27 10:39:24 -0700

    Update homework.asm

--- assembly/homework/homework.asm
+++ assembly/homework/homework.asm
@@ -1,10 +1,18 @@
 //==========================================================================================
 // PROBLEM 1: Counting Up From 1 to 10 in Little Man Assembly
 //==========================================================================================
-
-// You can develop your code via the online emulator and then paste it in here
-// Please use a jump in your solution
-// Please use the OUT instruction to print values
+      
+        LDA ONE
+        SUB TEN
+LOOP    BRZ EXIT
+        ADD TEN
+        OUT
+        ADD ONE
+        SUB TEN
+        BRA LOOP
+EXIT    HLT
+ONE     DAT 1
+TEN     DAT 11
 
 //==========================================================================================
 // PROBLEM 2: Counting Up From 1 to 10 in MIPS Assembly
